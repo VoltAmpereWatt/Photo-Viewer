eb3538ce2c26b620b504a56d87e84f77e9a84241
Rahul <rrr392@nyu.edu>
2020-06-18 16:29:14 -0400

    cba

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const dbuser = 'rahul';
 const dbpassword = 'rahul123';
 
-const MONGODB_URI = "mongodb://heroku_7pg50zqz:ajhkndefg1fq7u2gbnidb4100r@ds153093.mlab.com:53093/heroku_7pg50zqz";
+const MONGODB_URI = "mongodb+srv://rahul:rahul123@cluster0-844ck.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = MONGODB_URI;
 
