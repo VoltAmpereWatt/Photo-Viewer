4b3ad4590a0f623de772dcfdb939061eae084d37
Rahul <rrr392@nyu.edu>
2020-06-19 23:22:26 -0400

    cba

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ const dbpassword = 'rahul123';
 
 const MONGODB_URI = `mongodb+srv://${dbuser}:${dbpassword}@cluster0-844ck.gcp.mongodb.net/test?retryWrites=true&w=majority`;
 
-export default MONGODB_URI;
+module.exports = MONGODB_URI;
 
